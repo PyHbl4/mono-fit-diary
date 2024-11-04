8f6bfb345cc42fa9eee611c0752a5b45ff77c7ac
Maxim <runkov@jetstyle.ru>
2024-11-04 18:15:03 +0500

    added new fields

---
+++ apps/fitdiary-api/prisma/migrations/20241104131410_new_fields/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Users" ADD COLUMN     "birthDate" TIMESTAMP(3),
+ADD COLUMN     "gender" TEXT;
--- run-db.sh
+++ run-db.sh
@@ -1,7 +1,7 @@
 docker run -it --rm \
     -p 5432:5432 \
     --name pg-fitdiary \
-    -e POSTGRES_PASSWORD=p9X#mK2$qL7@vF3nR8 \
+    -e POSTGRES_PASSWORD=Gbplslfvofc123 \
     -e PGDATA=/var/lib/postgresql/data/pgdata \
     -v $PWD/../server-pgdata/pgdata:/var/lib/postgresql/data \
     postgres:17
