04ea6499cd30277cb9a2b09d4dc11c7f802d446f
Maxim <runkov@jetstyle.ru>
2024-11-04 18:33:50 +0500

    added swagger

--- apps/fitdiary-api/src/main.ts
+++ apps/fitdiary-api/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
+  const config = new DocumentBuilder().setTitle('fitness API').setDescription('API for fitDiary').setVersion('1.0').build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
   await app.listen(3000);
 }
 bootstrap();
